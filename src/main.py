eac7a09612b1a2bc67b7e0ca29c6815bced59607
Vadim Karimov <warqone12@yandex.ru>
2025-09-16 22:31:50 +0500

    init

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-# Это основной запускаемый файл.
-# Заменить содержимое своим кодом.
+from fastapi import FastAPI
 
-import sys
+from app.api import main_router
+from app.core.config import settings
 
-print(sys.path)
+
+app: FastAPI = FastAPI(
+    title=settings.app_title, description=settings.description
+)
+
+app.include_router(main_router)
