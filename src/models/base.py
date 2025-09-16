fe6a26e6e159a3fa5a94f14e3efdaeefb9222454
Dzmitry-Radziuk <mitia.radiuk@yandex.ru>
2025-09-16 23:23:55 +0300

    убрано дублирующее поле id  из BaseModel

--- src/models/base.py
+++ src/models/base.py
@@ -4,7 +4,6 @@ from src.core.db import Base
 
 class BaseModel(Base):
     __abstract__ = True
-    id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     active = Column(Boolean, default=True)
