45ed7b87d8ce2203567e7044e87253cfc4e8dc72
IskhakM <murzaev48@gmail.com>
2025-09-18 00:52:32 +0600

    fix

--- src/models/action.py
+++ src/models/action.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, Text, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.models.base import BaseModel
 
 
 class Actions(BaseModel):
-    description = Column(Text, nullable=False)
-    cafe_id = Column(Integer, ForeignKey('cafe.id'))
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    cafe_id: Mapped[int] = mapped_column(Integer, ForeignKey('cafes.id'))
