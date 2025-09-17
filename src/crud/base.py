68f3e107d9a1d53ecd12377069934510ea9e593a
IskhakM <murzaev48@gmail.com>
2025-09-17 16:12:58 +0600

    Добавлен CRUDBase

---
+++ src/crud/base.py
@@ -0,0 +1,75 @@
+from typing import Generic, Optional, TypeVar
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.db import Base
+from src.models import User
+
+ModelType = TypeVar('ModelType', bound=Base)
+CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
+
+
+class CRUDBase(Generic[ModelType, CreateSchemaType]):
+
+    def __init__(self, model):
+        self.model = model
+
+    async def get(
+            self,
+            obj_id: int,
+            session: AsyncSession
+    ):
+        db_obj = await session.execute(
+            select(self.model).where(
+                self.model.id == obj_id
+            )
+        )
+        return db_obj.scalars().first()
+
+    async def get_multi(
+            self,
+            session: AsyncSession,
+            active: Optional[bool] = True
+    ):
+        response = select(self.model)
+
+        if active is not None:
+            response = response.where(self.model.active == active)
+
+        db_objects = await session.execute(response)
+        return db_objects.scalars().all()
+
+    async def create(
+            self,
+            obj_in,
+            session: AsyncSession,
+            user: Optional[User] = None
+    ):
+        obj_in_data = obj_in.dict()
+        if user is not None:
+            obj_in_data['user_id'] = user.id
+        db_obj = self.model(**obj_in_data)
+        session.add(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
+        return db_obj
+
+    async def update(
+            self,
+            db_obj,
+            obj_in,
+            session: AsyncSession
+    ):
+        obj_data = jsonable_encoder(db_obj)
+        update_data = obj_in.dict(exclude_unset=True)
+
+        for field in obj_data:
+            if field in update_data:
+                setattr(db_obj, field, update_data[field])
+        session.add(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
+        return db_obj
