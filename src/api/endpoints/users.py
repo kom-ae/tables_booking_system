917eeb7b78a10358445568d3dbfee3266f72b07d
IskhakM <138295514+IskhakM@users.noreply.github.com>
2025-09-18 01:13:43 +0600

    Update users.py

--- src/api/endpoints/users.py
+++ src/api/endpoints/users.py
@@ -1,5 +1,5 @@
-from app.core.user import auth_backend, fastapi_users
-from app.schemas.user import UserCreate, UserRead, UserUpdate
+from src.core.user import auth_backend, fastapi_users
+from src.schemas.user import UserCreate, UserRead, UserUpdate
 from fastapi import APIRouter
 
 router = APIRouter()
