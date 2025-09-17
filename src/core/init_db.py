87c5e5df3fe22a53956ef9ebcee20f713253885a
Dzmitry-Radziuk <mitia.radiuk@yandex.ru>
2025-09-18 00:39:12 +0300

    Добавлено автоматическое создание суперпользователя при запуске uvicorn

--- src/core/init_db.py
+++ src/core/init_db.py
@@ -1,28 +1,19 @@
-from contextlib import asynccontextmanager
-from typing import AsyncGenerator, Optional
+from typing import Optional
 
 from fastapi_users.password import PasswordHelper
 from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.config import settings
 from src.core.db import get_async_session
 from src.models.user import User
 
 
-# Асинхронный контекстный менеджер для сессии
-@asynccontextmanager
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    """Асинхронный контекстный менеджер для работы с базой данных."""
-    async with get_async_session() as session:
-        yield session
-
-
 async def create_first_superuser() -> None:
     """Создаёт суперпользователя напрямую через базу, если его нет."""
     password_helper = PasswordHelper()
 
-    async with get_session() as session:
+    # Используем get_async_session как асинхронный генератор напрямую
+    async for session in get_async_session():
         # Проверяем, существует ли уже пользователь с таким email
         result = await session.execute(
             select(User).where(User.email == settings.first_superuser_email),
