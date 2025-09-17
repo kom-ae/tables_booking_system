8d3bd2a5c8a2ed878f5ef11d366999f415ea20df
Dzmitry-Radziuk <mitia.radiuk@yandex.ru>
2025-09-18 01:20:28 +0300

    Добавлено автоматическое создание таблиц и^Cуперпользователя при запуске uvicorn

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ from fastapi import FastAPI
 
 from src.api import main_router
 from src.core.config import settings
-from src.core.init_db import create_first_superuser
+from src.core.init_db import create_first_superuser, init_db
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     """Создаёт суперпользователя при старте приложения."""
+    await init_db()
     await create_first_superuser()
     yield
 
