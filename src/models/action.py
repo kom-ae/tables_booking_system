d4d0344604f568e1dae6e2557b3077c17053a4ab
IskhakM <murzaev48@gmail.com>
2025-09-17 20:15:27 +0600

    little fix

--- src/models/action.py
+++ src/models/action.py
@@ -4,6 +4,5 @@ from src.models.base import BaseModel
 
 
 class Actions(BaseModel):
-    id = Column(Integer, primary_key=True)
     description = Column(Text, nullable=False)
     cafe_id = Column(Integer, ForeignKey('cafe.id'))
