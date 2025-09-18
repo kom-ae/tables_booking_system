ea039f500d2f0bbdf53a846f87e4dbaae5444688
IskhakM <murzaev48@gmail.com>
2025-09-18 18:55:01 +0600

    Дописал схему для Actions

--- src/schemas/action.py
+++ src/schemas/action.py
@@ -1,22 +1,34 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from src.schemas.base import BaseSchema
+from src.schemas.cafes import CafeBase, CafeUpdate
 
 
-class ActionsBase(BaseSchema):
-    cafe: Optional[dict] = None
-    description: str
+class ActionsBase(BaseModel):
+    """Базовая схема для акций."""
+
+    cafe: CafeBase = Field(..., title='Кафе')
+    description: str = Field(..., title='Описание акции')
 
 
-class ActionsCreate(BaseModel):
+class ActionsCreate(ActionsBase):
+    """Схема для создания акций"""
     pass
 
 
-class ActionsUpdate(ActionsCreate):
-    pass
+class ActionsUpdate(ActionsBase):
+    """Схема для изменения акций"""
+    cafe: Optional[CafeUpdate] = Field(None, title='Кафе')
+    description: Optional[str] = Field(None, title='Описание акции')
 
 
 class ActionsDB(ActionsBase, BaseSchema):
+    """Возвращаемая схема акций."""
     pass
+
+    class Config:
+        """Конфиг класса."""
+
+        orm_mode = True
