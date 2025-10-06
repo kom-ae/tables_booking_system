77e596be00bb0febca4adb6b35549f28c41c7ee1
Vadim Karimov <warqone12@yandex.ru>
2025-10-07 01:21:52 +0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,7 @@
-set -e
+#!/bin/sh
 
-echo "🚀 Запуск миграций базы данных..."
-alembic upgrade head || true
-
-echo "👤 Создание суперпользователя (если нужно)..."
-create_superuser || true
-
-case "$1" in
-  celery)
-    echo "📦 Запуск Celery worker..."
-    shift
-    exec celery -A src.celery.celery_app.celery_app worker --loglevel=info "$@"
-    ;;
-  flower)
-    echo "🌸 Запуск Flower..."
-    shift
-    exec celery -A src.celery.celery_app.celery_app flower --port=5555 "$@"
-    ;;
-  *)
-    echo "🖥 Запуск FastAPI (Uvicorn)..."
-    exec uvicorn src.main:app --host 0.0.0.0 --port 8000 "$@"
-    ;;
-esac
+echo "Запуск миграций базы данных..."
+alembic upgrade head
+create_superuser
+uvicorn src.main:app --host 0.0.0.0 --port 8000
+celery -A src.celery.app.celery_app worker --loglevel=info
